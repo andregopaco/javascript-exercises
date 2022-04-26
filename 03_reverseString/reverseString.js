fbfff7eadf6509dba7b99a1e7ba1e6838efe642e
André Gomes Pacheco <andregp86@gmail.com>
2022-04-26 19:55:11 -0300

    Create fully tested function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let charCount = word.length;
+    let reversedWord = "";
+    for (let i = charCount; i >= 0; i--) {
+        let char = word.charAt(i);
+        reversedWord += char;
+    }
+    return reversedWord;
 };
+
+// x = reverseString("example");
+// console.log(x);
 
 // Do not edit below this line
 module.exports = reverseString;
