ccd4959890095a44e18287296973748ff12c3e72
André Gomes Pacheco <andregp86@gmail.com>
2022-04-25 23:26:27 -0300

    Create funcion that passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (i = 0; i < num; ++i) {
+            output += string;
+        }
+        return output;
+    }
 };
+
+repeatString("yeah", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
