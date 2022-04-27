bdf3e6234956fc40ad46787ad935817dc22236fe
André Gomes Pacheco <andregp86@gmail.com>
2022-04-26 22:44:43 -0300

    Add first version of requested function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputtedArray, ...args) {
+    let argIndex;
+    let argsArray = [...args];
+    for (arg in argsArray) {
+        let argIndex = inputtedArray.indexOf(arg);
+        inputtedArray.splice(argIndex, 1);
+    }
+    return inputtedArray;
 };
+
+
+//     for (arg in argsArray) {
+//        for (item in inputtedArray) {
+//            if (arg === item) {
+                   
+//            }
+//        }
+//     }
+//     return inputtedArray;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
