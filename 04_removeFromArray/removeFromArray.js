26dccb30dc3fa492a2ab80d27b8fa9d17aa1fa56
André Gomes Pacheco <andregp86@gmail.com>
2022-04-26 23:18:24 -0300

    End task and add a comment on function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,11 @@
 const removeFromArray = function(inputtedArray, ...args) {
-    let argIndex;
     let argsArray = [...args];
     for (let arg of argsArray) {
         let argIndex = inputtedArray.indexOf(arg);
-        inputtedArray.splice(argIndex, 1);
+        // If an inputted value is not present, its index will be -1. Therefore:
+        if (argIndex > -1) {
+            inputtedArray.splice(argIndex, 1);
+        }
     }
     return inputtedArray;
 };
