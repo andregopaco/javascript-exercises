8cf059f59695ea8b414b7ed6ffedd589e5f285ac
André Gomes Pacheco <andregp86@gmail.com>
2022-04-26 23:08:21 -0300

    Change for in to for of loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,12 @@
 const removeFromArray = function(inputtedArray, ...args) {
     let argIndex;
     let argsArray = [...args];
-    for (arg in argsArray) {
+    for (let arg of argsArray) {
         let argIndex = inputtedArray.indexOf(arg);
         inputtedArray.splice(argIndex, 1);
     }
     return inputtedArray;
 };
-
-
-//     for (arg in argsArray) {
-//        for (item in inputtedArray) {
-//            if (arg === item) {
-                   
-//            }
-//        }
-//     }
-//     return inputtedArray;
-// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
